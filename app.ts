76bdb74a170913e766f3503114b1f4f3f5d20c03
Robert Haskell <rjhaskell@gmail.com>
2017-03-08 23:43:09 -0800

    Implement stretch feature 5

--- app.ts
+++ app.ts
@@ -1,186 +1,150 @@
 //Question class w/constructor
 class Question {
     constructor(public ask: string,
-                public ans01: string,
-                public ans02: string,
-                public ans03: string,
-                public correctAnswer: string,
-                public points: number = 20) {
+        public answerChoice1: string,
+        public answerChoice2: string,
+        public answerChoice3: string,
+        public correctAnswer: string,
+        public points: number = 20) {
     }
 }
 
-//5 questions build
-let q1 = new Question("What is the capital of Texas?", "A. Dallas", "B. Houston", "C. Austin", "C", 20);
-let q2 = new Question("What is the opposite of down?", "A. Up", "B. Left", "C. Right", "A", 20);
-let q3 = new Question("What is 2+2?", "A. 2", "B. 3", "C. 4", "C", 20);
-let q4 = new Question("Blue and yellow makes what?", "A. Green", "B. Brown", "C. Red", "A", 20);
-let q5 = new Question("How many sides to a square?", "A. 4", " B. 3", "C. 2", "A", 20);
+//Question bank with answer choices, correct answer, and initial point values
+let q01 = new Question("What is the capital of Texas?", "A. Dallas", "B. Houston", "C. Austin", "C", 20);
+let q02 = new Question("What is the opposite of down?", "A. Up", "B. Left", "C. Right", "A", 20);
+let q03 = new Question("What is 2+2?", "A. 2", "B. 3", "C. 4", "C", 20);
+let q04 = new Question("Blue and yellow makes what?", "A. Green", "B. Brown", "C. Red", "A", 20);
+let q05 = new Question("How many sides to a square?", "A. 4", " B. 3", "C. 2", "A", 20);
 
-let quizScore = 0; 
+//Initailize running total score
+let quizScore = 0;
 
-//Question 1 prompt to user
-let result1: string = window.prompt(`Question 1:   (${q1.points} points)
+//Each question has it's own function 1111111111111111111111111111111111111111
+function question01() {
 
-    ${q1.ask}
-        ${q1.ans01}
-        ${q1.ans02}
-        ${q1.ans03}`,
-`Your answer: A, B, or C`);
+    //Promopt user for answer
+    let result01: string = window.prompt(`Question 1:   (${q01.points} points)
 
+    ${q01.ask}
+        ${q01.answerChoice1}
+        ${q01.answerChoice2}
+        ${q01.answerChoice3}`,
+        `Your answer: A, B, or C (NOT case sensitive)`);
 
-//Process user answer
-let _result1 = result1.toUpperCase();
+    //In-process user answer
+    let _result01 = result01.toUpperCase();
 
-if (_result1 === q1.correctAnswer){
+    //Show positive result 
+    if (_result01 === q01.correctAnswer) {
 
-    quizScore = quizScore + q1.points;
-    
-    alert(`That's correct!
+        //Increase running total score by current question value
+        quizScore = quizScore + q01.points;
+        alert(`That's correct!
 
-    You earned ${q1.points} points.
+    You earned (${q01.points}) points.
     Total quiz score so far: ${quizScore}`);
+    }
 
-} else {
-    alert(`Oops... wrong answer.
+    //Show negative result//
+     else {
+        
+        //Reduce question value by 1 and ....
+        q01.points = q01.points - 1;
+        alert(`Oops... wrong answer.
 
-    You earned no points for this question.
+    Hit "OK" to try again.
+
     Total quiz score so far: ${quizScore}`);
-}
+        //.... try again
+        question01();
+    };
+};
 
+//2222222222222222222222222222222222222222222222222222222222222222222222222222
+function question02() {
 
-console.log(_result1);
-console.log(q1.correctAnswer);
+    //Promopt user for answer
+    let result02: string = window.prompt(`Question 2:   (${q02.points} points)
 
-//Question 2 prompt to user
-let result2 = window.prompt(`Question 2:   (${q2.points} points)
+    ${q02.ask}
+        ${q02.answerChoice1}
+        ${q02.answerChoice2}
+        ${q02.answerChoice3}`,
+        `Your answer: A, B, or C (NOT case sensitive)`);
 
-    ${q2.ask}
-        ${q2.ans01}
-        ${q2.ans02}
-        ${q2.ans03}`,
-`Your answer: A, B, or C`);
+    //In-process user answer
+    let _result02 = result02.toUpperCase();
 
+    //Show positive result 
+    if (_result02 === q02.correctAnswer) {
 
-//Process user answer
-let _result2 = result2.toUpperCase();
+        //Increase running total score by current question value
+        quizScore = quizScore + q02.points;
+        alert(`That's correct!
 
-if (_result2 === q2.correctAnswer){
-
-    quizScore = quizScore + q2.points;
-    
-    alert(`That's correct!
-
-    You earned ${q2.points} points.
+    You earned (${q02.points}) points.
     Total quiz score so far: ${quizScore}`);
+    }
 
-    //console.log(quizScore);
-} else {
-    alert(`Oops... wrong answer.
+    //Show negative result//
+     else {
+        
+        //Reduce question value by 1 and ....
+        q02.points = q02.points - 1;
+        alert(`Oops... wrong answer.
 
-    You earned no points for this question.
+    Hit "OK" to try again.
+
     Total quiz score so far: ${quizScore}`);
-}
+        //.... try again
+        question02();
+    };
+};
 
+//3333333333333333333333333333333333333333333333333333333333333333333333333333
+function question03() {
 
-console.log(_result2);
-console.log(q2.correctAnswer);
+    //Promopt user for answer
+    let result03: string = window.prompt(`Question 3:   (${q03.points} points)
 
-//Question 3 prompt to user
-let result3: string = window.prompt(`Question 3:   (${q3.points} points)
+    ${q03.ask}
+        ${q03.answerChoice1}
+        ${q03.answerChoice2}
+        ${q03.answerChoice3}`,
+        `Your answer: A, B, or C (NOT case sensitive)`);
 
-    ${q3.ask}
-        ${q3.ans01}
-        ${q3.ans02}
-        ${q3.ans03}`,
-`Your answer: A, B, or C`);
+    //In-process user answer
+    let _result03 = result03.toUpperCase();
 
+    //Show positive result 
+    if (_result03 === q03.correctAnswer) {
 
-//Process user answer
-let _result3 = result3.toUpperCase();
+        //Increase running total score by current question value
+        quizScore = quizScore + q03.points;
+        alert(`That's correct!
 
-if (_result3 === q3.correctAnswer){
-
-    quizScore = quizScore + q3.points;
-    
-    alert(`That's correct!
-
-    You earned ${q3.points} points.
+    You earned (${q03.points}) points.
     Total quiz score so far: ${quizScore}`);
+    }
 
-    //console.log(quizScore);
-} else {
-    alert(`Oops... wrong answer.
+    //Show negative result//
+     else {
+        
+        //Reduce question value by 1 and ....
+        q03.points = q03.points - 1;
+        alert(`Oops... wrong answer.
 
-    You earned no points for this question.
+    Hit "OK" to try again.
+
     Total quiz score so far: ${quizScore}`);
-}
+        //.... try again
+        question03();
+    };
+};
 
+question01();
+question02();
+question03();
 
-console.log(_result3);
-console.log(q3.correctAnswer);
-
-//Question 4 prompt to user
-let result: string = window.prompt(`Question 4:   (${q4.points} points)
-
-    ${q4.ask}
-        ${q4.ans01}
-        ${q4.ans02}
-        ${q4.ans03}`,
-`Your answer: A, B, or C`);
-
-
-//Process user answer
-let _result4 = result.toUpperCase();
-
-if (_result4 === q4.correctAnswer){
-
-    quizScore = quizScore + q4.points;
-    
-    alert(`That's correct!
-
-    You earned ${q4.points} points.
-    Total quiz score so far: ${quizScore}`);
-
-    //console.log(quizScore);
-} else {
-    alert(`Oops... wrong answer.
-
-    You earned no points for this question.
-    Total quiz score so far: ${quizScore}`);
-}
-
-
-console.log(_result4);
-console.log(q4.correctAnswer);
-
-//Question 5 prompt to user
-let result5: string = window.prompt(`Question 5:   (${q5.points} points)
-
-    ${q5.ask}
-        ${q5.ans01}
-        ${q5.ans02}
-        ${q5.ans03}`,
-`Your answer: A, B, or C`);
-
-
-//Process user answer
-let _result5 = result.toUpperCase();
-
-if (_result5 === q5.correctAnswer){
-
-    quizScore = quizScore + q5.points;
-    
-    alert(`That's correct!
-
-    You earned ${q5.points} points.
-    Total quiz score so far: ${quizScore}`);
-
-    //console.log(quizScore);
-} else {
-    alert(`Oops... wrong answer.
-
-    You earned no points for this question.
-    Total quiz score so far: ${quizScore}`);
-}
 alert(`That all folks....
 Final quiz score : ${quizScore}`);
